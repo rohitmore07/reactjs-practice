7f12b61b85909fcddf6210b0f5b3af4380b7bed6
Rohit More <cserohitmore@gmail.com>
2024-10-15 02:27:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <p>Hello World! Welcome to ReactJS.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Main Navbar hu</div>
+    </div>
+  )
+}
+
+export default Navbar
